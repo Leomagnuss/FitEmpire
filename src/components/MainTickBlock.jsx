fd0eb61aa4d86be506a6acaa92ced8daac55db73
Leo <sokolov.nikita01@list.ru>
2024-06-10 15:28:11 +0300

    second

--- src/components/MainTickBlock.jsx
+++ src/components/MainTickBlock.jsx
@@ -6,6 +6,12 @@ const TitleBlock = styled.div`
     align-items: center;
 `
 
+const TickBlock = styled.div`
+    display:flex;
+    justify-content:space-around;
+    align-items: flex-start;
+`
+
 const Title = styled.h1`
     font-size: 71px;
     color: transparent;
@@ -16,10 +22,40 @@ const Title = styled.h1`
     font-weight: 900;
 `
 
-const TickBlock = styled.div`
-    display:flex;
-    justify-content:space-around;
-    align-items: flex-start;
+const TitleTextTick = styled.div`
+    display: grid;
+    margin-top: 80px;
+`
+
+const TickTextBlock = styled.div`
+    display: flex;
+    align-items: center;
+    padding-top: 50px;
+`
+
+const TickPic = styled.div`
+    display: grid;
+`
+
+const Tick = styled.div`
+    padding-bottom: 39px;
+`
+
+const TextTick = styled.div`
+    
+`
+
+const TextIn = styled.div`
+    font-size: 29px;
+    font-family: "Roboto";
+    font-weight: 600;
+    font-style: italic;
+    padding:0 0 49px 25px;
+    color: #ffffff;
+`
+
+const TextInColor = styled.span`
+    color: #ff4d00;
 `
 
 const TickPhotoBlock = styled.div`
@@ -30,11 +66,25 @@ const MainTickBlock = () => {
   return (
     <div>
         <TickBlock>
-            <TitleBlock>
-                <Title>Freedom</Title>
-                <img src="./images/Kettlebell.svg" alt="" />   
-            </TitleBlock>
-        <TickPhotoBlock><img src="./images/Tickimage.svg" alt="" /></TickPhotoBlock>
+            <TitleTextTick>
+                <TitleBlock>
+                    <Title>Freedom</Title>
+                    <img src="./images/Kettlebell.svg" alt="" />   
+                </TitleBlock>
+                <TickTextBlock>
+                    <TickPic>
+                    <Tick><img src="./images/TickPic.svg" alt="" /></Tick>
+                    <Tick><img src="./images/TickPic.svg" alt="" /></Tick>
+                    <Tick><img src="./images/TickPic.svg" alt="" /></Tick>
+                    </TickPic>
+                    <TextTick>
+                        <TextIn>Complete freedom of your <TextInColor>training.</TextInColor></TextIn>
+                        <TextIn><TextInColor>Trane</TextInColor> alone or in a group.</TextIn>
+                        <TextIn>On your own or with a <TextInColor>professional.</TextInColor></TextIn>
+                    </TextTick>
+                </TickTextBlock>
+            </TitleTextTick>
+            <TickPhotoBlock><img src="./images/Tickimage.svg" alt="" /></TickPhotoBlock>
         </TickBlock>
         
     </div>
